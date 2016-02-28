621191126d89af2323badc7f362867860b71cd49
FEFalk <fille_945@hotmail.com>
2016-02-28 17:46:31 +0100

    init2

--- Source Files/client.cpp
+++ Source Files/client.cpp
@@ -1,68 +1,100 @@
-#include <stdio.h>
+
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include <unistd.h>
+#include "shared.h"
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
-}
+#define SERVER_IP "127.0.0.1"
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+    struct sockaddr_in si_server, si_other;
+    int s, i;
+    socklen_t slen=sizeof si_server;
+    int uniqueIdentifier, windowSize;
+    char *errorMessage;
+    int nbytes;
+    rtp *sendPacket = (rtp *)calloc(sizeof(rtp), 1);
+    sendPacket->data = (char *)calloc(sizeof(char), 256);
+    rtp recvPacket;
+
+
+    strcpy(sendPacket->data, "Hello 1 :^)");
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+    {
+        strcpy(errorMessage, "Failed to create socket");
+        diep(errorMessage);
+    }
+
+
+    memset((char *) &si_server, 0, sizeof(si_server));
+
+    si_server.sin_family = AF_INET;
+    si_server.sin_addr.s_addr = inet_addr(SERVER_IP);
+    si_server.sin_port = htons(PORT);
+
+    if (inet_aton(SERVER_IP, &si_server.sin_addr)==0) {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
+
+
+    sendPacket->flags=SYN;
+    sendPacket->id=0;
+    nbytes = sendto(s, (void *) sendPacket, sizeof(*sendPacket), 0, (struct sockaddr*) &si_server, slen);
+
+    printf("%d\n", nbytes);
 
     char buffer[256];
 
-    if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
+    recvfrom(s, &recvPacket, 256, 0, (struct sockaddr*) &si_other, &slen);
+
+    if(recvPacket.flags==SYN_ACK)
+    {
+        uniqueIdentifier=recvPacket.id;
+        windowSize=recvPacket.windowsize;
+
+        sendPacket->flags=ACK;
+        nbytes = sendto(s, (void *) sendPacket, sizeof(*sendPacket), 0, (struct sockaddr*) &si_server, slen);
     }
 
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("%d\n", nbytes);
+    //TODO
+    /*If timed out, proceed*/
+    //recvfrom(s, &recvPacket, sizeof(rtp), 0, (struct sockaddr*) &si_other, (socklen_t *)&slen);
+    //if(recvPacket.flags==ACK)
+    //{
+    //    printf("Connected to the server!");
+    //}
 
-    if (sockfd < 0)
-        error("ERROR opening socket");
+    sendPacket->flags=DATA;
+    printf("Sending packet 1\n");
+    strcpy(sendPacket->data, "Hello 1 :^)");
+    //Change size to something more logical
+    nbytes = sendto(s, (void *)sendPacket, 256, 0, (struct sockaddr*) &si_server, slen);
 
-    server = gethostbyname(argv[1]);
-
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+    printf("%d\n", nbytes);
+    for (i=2; i<NPACK; i++) {
+        recvfrom(s, &recvPacket, sizeof(rtp), 0, (struct sockaddr*) &si_other, (socklen_t *)&slen);
+        if(recvPacket.flags==ACK)
+        {
+            printf("Sending packet %d\n", i);
+            fflush(stdout);
+            sprintf(sendPacket->data, "Hello %d :^)", i);
+            if((sendto(s, (void*) sendPacket, 256, 0, (struct sockaddr*) &si_server, slen)) == -1)
+            {
+                strcpy(errorMessage, "Failed the 'sendto()'");
+                diep(errorMessage);
+            }
+            printf("%s", errorMessage);
+        }
     }
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-
-    if (n < 0)
-        error("ERROR writing to socket");
-
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-
-    if (n < 0)
-        error("ERROR reading from socket");
-
-
-    printf("%s\n",buffer);
-    close(sockfd);
+    close(s);
     return 0;
 }
---
+++ Source Files/shared.cpp
@@ -0,0 +1,12 @@
+//
+// Created by fille on 2016-02-26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void diep(char *s)
+{
+    perror(s);
+    exit(1);
+}
