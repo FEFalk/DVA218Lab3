27a2690b29c43954dc0122e963efa50df920170a
frenning <fredrikfrenning@gmail.com>
2016-02-29 16:21:28 +0100

    server files + recvdatafrom

--- Source Files/wrapper.cpp
+++ Source Files/wrapper.cpp
@@ -29,8 +29,6 @@ void recvDataFrom(int s, char *msg, int windowsize, int packetSize, struct socka
 
     rtp *sendPacket = (rtp *)calloc(sizeof(rtp), 1);
 
-
-
     //CHECK SEQUENCE NUMBER STORE IN BUFF SEND ACK
 
     while(1)
@@ -62,4 +60,51 @@ void recvDataFrom(int s, char *msg, int windowsize, int packetSize, struct socka
         }
         break;
     }
+
+}
+
+void connectTo(int s, struct sockaddr_in si_client, int *uniqueIdentifier)
+{
+    struct sockaddr_in si_other;
+    rtp *sendPacket = (rtp *)calloc(sizeof(rtp), 1);
+    rtp *recvPacket = (rtp *)calloc(sizeof(rtp), 1);
+    socklen_t slen=sizeof si_client;
+
+    bool connected=false;
+    while(!connected)
+    {
+        recvfrom(s, &recvPacket, sizeof(rtp), 0, (struct sockaddr*) &si_other, &slen);
+        if(recvPacket->flags==SYN)
+        {
+            sendPacket->flags=SYN_ACK;
+            sendPacket->id=++*uniqueIdentifier;
+            cout << "Received SYN from client.\n";
+            while(1)
+            {
+                sendto(s, (void *) sendPacket, sizeof(*sendPacket), 0, (struct sockaddr*) &si_client, slen);
+                cout << "Waiting to connect...\n";
+                if(recvfrom(s, &recvPacket, sizeof(rtp), 0, (struct sockaddr*) &si_other, &slen) < 0)
+                {
+                    if(sendPacket->flags==SYN_ACK)
+                    {
+                        cout << "Timeout reached. Resending connection request (SYN)." << ".\n";
+                        continue;
+                    }
+                }
+                else if(recvPacket->flags==SYN) {
+                    cout << "Connection was interrupted by SYN+ACK packet from server. Resending ACK...\n";
+                    continue;
+                }
+                else if(recvPacket->flags == ACK)
+                {
+                    connected=true;
+                    break;
+                }
+                else
+                    continue;
+            }
+        }
+
+    }
+
 }
--- Source Files/wrapper.h
+++ Source Files/wrapper.h
@@ -6,6 +6,7 @@
 #define SERVER_WRAPPER_H
 
 void recvDataFrom(int s, char *msg, int windowsize, int packetSize, struct sockaddr_in si_client);
+void connectTo(int s, struct sockaddr_in si_client, int *uniqueIdentifier);
 void terminateProgram(int s);
 
 #endif //SERVER_WRAPPER_H
